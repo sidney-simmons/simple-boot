81770b1e2ec0a8367c36b69715efe69b630b1083
Sidney Simmons <sas.simm@gmail.com>
2019-08-10 12:40:55 -0600

    #5 Add simple dashboard template

---
+++ src/main/java/com/sidneysimmons/simple/boot/lottery/controller/DashboardController.java
@@ -0,0 +1,26 @@
+package com.sidneysimmons.simple.boot.lottery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Dashboard controller for dashboard related endpoints.
+ * 
+ * @author Sidney Simmons
+ */
+@Controller
+@RequestMapping(value = "/dashboard")
+public class DashboardController {
+
+	/**
+	 * Return the dashboard page.
+	 * 
+	 * @return the dashboard page
+	 */
+	@GetMapping(value = "")
+	public String createNumbers() {
+		return ViewNames.DASHBOARD;
+	}
+
+}
---
+++ src/main/java/com/sidneysimmons/simple/boot/lottery/controller/ViewNames.java
@@ -0,0 +1,16 @@
+package com.sidneysimmons.simple.boot.lottery.controller;
+
+/**
+ * Class to hold view names.
+ * 
+ * @author Sidney Simmons
+ */
+public class ViewNames {
+
+	public static final String DASHBOARD = "dashboard";
+
+	private ViewNames() {
+		// No need to instantiate this class
+	}
+
+}
